862331331660301e1229d7e4ea85da828284e4f4
Devfrank94 <francescomurro94@gmail.com>
2023-05-18 17:17:07 +0200

    partial commit

---
+++ create-json.php
@@ -0,0 +1,18 @@
+<?php
+$tasks = [
+  [ 
+    'task' => 'Fai la spesa',
+    'check' => 'false',
+  ],
+  [ 
+    'task' => 'Compra un libro',
+    'check' => 'false',
+  ],
+  [ 
+    'task' => 'Vai in palestra',
+    'check' => 'false',
+  ]
+
+];
+
+?>
